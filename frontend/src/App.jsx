5ff18daa75e7b90a56bdf65a7f7542ec0848e5d2
KarakayaMetin8787 <karakayametin8787@gmail.com>
2024-02-07 15:48:52 +0100

    added detailpage for boats

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Boote from "./pages/boote/Boote";
 import Dashboard from "./pages/dashboard/Dashbaord";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Reservierungen from "./pages/reservierungen/Reservierungen";
+import BootDetail from "./pages/bootdetail/BootDetail";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Dashboard />}/>
           <Route path="/boote" element={<Boote/>}/>
           <Route path="/reservierungen" element={<Reservierungen/>}/>
+          <Route path="/boote/:id" element={<BootDetail/>}/>
         </Routes>
       </BrowserRouter>
     </>
