24ac2127443f3a74377b91297e63a500ffaedaf6
SuperCoderSuat <supercodesuat@gmail.com>
2024-02-07 15:47:25 +0100

    added addNewBoat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Boote from "./pages/boote/Boote";
 import Dashboard from "./pages/dashboard/Dashbaord";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Reservierungen from "./pages/reservierungen/Reservierungen";
+import NeuesBoot from "./pages/neuesboot/NeuesBoot";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />}/>
           <Route path="/boote" element={<Boote/>}/>
+          <Route path="/boote/neuesboot" element={<NeuesBoot/>}/>
           <Route path="/reservierungen" element={<Reservierungen/>}/>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/neuesboot/NeuesBoot.jsx
+++ frontend/src/pages/neuesboot/NeuesBoot.jsx
@@ -1,14 +1,62 @@
 import "./NeuesBoot.scss";
+import { addNewBoat } from "../../api/fetchBoote";
+import { useState } from "react";
 
 const NeuesBoot = () => {
-  return (
-    <>
-      <section className="content_layout">
-        <article className="content">
+  const [boatDetails, setBoatDetails] = useState({
+    baujahr: '',
+    seriennummer: '',
+    material: '',
+    bootsart: '',
+  });
 
-        </article>
-      </section>
-    </>
+  const [notification, setNotification] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBoatDetails({ ...boatDetails, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await addNewBoat(boatDetails);
+      console.log('Neues Boot erfolgreich erstellt:', response);
+      setNotification('Boot erfolgreich angelegt.'); // Benachrichtigung setzen
+      setBoatDetails({
+        baujahr: '',
+        seriennummer: '',
+        material: '',
+        bootsart: '',
+      }); // Eingabefelder leeren
+    } catch (error) {
+      console.error('Fehler beim Anlegen des neuen Bootes:', error);
+      setNotification('Fehler beim Anlegen des Boots.'); // Benachrichtigung setzen
+    }
+  };
+
+  return (
+    <section className="content_layout">
+      <article className="content">
+        <h2>Neues Boot anlegen</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form_layout">
+              <input className="form_input" type="text" name="baujahr" value={boatDetails.baujahr} onChange={handleChange} placeholder="Baujahr"/>
+          </div>
+          <div className="form_layout">
+              <input className="form_input" type="text" name="seriennummer" value={boatDetails.seriennummer} onChange={handleChange} placeholder="Seriennummer"/>
+          </div>
+          <div className="form_layout">
+              <input className="form_input" type="text" name="material" value={boatDetails.material} onChange={handleChange} placeholder="Material"/>
+          </div>
+          <div className="form_layout">
+              <input className="form_input" type="text" name="bootsart" value={boatDetails.bootsart} onChange={handleChange} placeholder="Bootsart"/>
+          </div>
+          <button className="form_submitbtn" type="submit">Neues Boot anlegen</button>
+        </form>
+        {notification && <p>{notification}</p>} {/* Benachrichtigung anzeigen */}
+      </article>
+    </section>
   );
 };
 
