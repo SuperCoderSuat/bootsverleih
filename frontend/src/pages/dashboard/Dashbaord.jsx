a1acddad76485697f838712a74c58640f2da1783
KarakayaMetin8787 <karakayametin8787@gmail.com>
2024-02-07 13:54:06 +0100

    added logic on dashboard. not complete

--- frontend/src/pages/dashboard/Dashbaord.jsx
+++ frontend/src/pages/dashboard/Dashbaord.jsx
@@ -1,32 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.scss";
 import { getAllBoats } from "../../api/fetchBoote.js";
 
 const Dashboard = () => {
 
+  const [fetchData, setFetchData] = useState([])
+  const [numberReservierung, setNumberReservierung] = useState("")
+  const [numberVerfuegbar, setNumberVerfuegbar] = useState("")
+  const [numberGesamt, setnumberGesamt] = useState("")
+
+
   useEffect(() => {
     const getData = async () => {
       try {
-        const data = await getAllBoats(); // fetchData aufrufen und auf die Daten warten
-        console.log(data); // Hier kannst du mit den erhaltenen Daten arbeiten
+        const data = await getAllBoats();
+        setFetchData(data.result);
+
+        //setting numberGesamt value:
+        const checkNumberGesamt = () => {
+          setnumberGesamt(fetchData.length)
+        }
+        checkNumberGesamt()
+
+
+        // const checkNumberVerfuegbar = () => {
+        //   const verfuegbareBoote = fetchData.filter((boot) =>
+        //     boot.termine.every((item) =>
+        //       Object.values(item).every((value) => value === true)
+        //     )
+        //   );
+        //   setNumberVerfuegbar(verfuegbareBoote.length);
+        // };
+        // checkNumberVerfuegbar();
       } catch (error) {
         console.log('Fehler beim Abrufen der Daten:', error);
       }
     };
-    getData(); // getData aufrufen
-  }, []);
+    getData();
+  }, [numberGesamt]);
 
+console.log(fetchData);
   return (
     <section className="content_layout">
-      <article className="content">
+      <article className="dashboardContent">
         <div className="icons reserv">Reservierungen
-        <div className="statisticNumbers" >1</div>
+        <div className="statisticNumbers" >{numberReservierung}</div>
         </div>
         <div className="icons verfueg">Verfügbare Boote
-        <div className="statisticNumbers" >2</div>
+        <div className="statisticNumbers" >{numberVerfuegbar}</div>
         </div>
         <div className="icons gesamt">Boote Gesamt
-        <div className="statisticNumbers" >3</div>
+        <div className="statisticNumbers" >{numberGesamt}</div>
         </div>
       </article>
     </section>
